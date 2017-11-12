b6769d4606049a8b9207c887c5bc610467d166cd
曹又心 <c_lisheng@163.com>
2017-11-12 17:42:32 +0800

    提现监控

---
+++ biz/withdraw/index.js
@@ -0,0 +1,70 @@
+function onPay(e, data) {
+  if (data.payed) {
+    return;
+  }
+
+  var confirmed = prompt("确定用户收到打款了吗？");
+  if (!confirmed) {
+    return;
+  }
+
+  get('/tbk/withdraw/response/' + data.id).done(responseMapper(renderOne));
+}
+
+function renderOne(isNew, data) {
+  var table = document.getElementById("table");
+
+  var handlers = [{
+    text: '已支付',
+    handler: onPay
+  }];
+
+  var renders = [null, {
+    innerHTML: renderUser
+  }, null, null, {
+      innerHTML: renderPayed
+    }];
+
+  var rowElem = buildRow(handlers, data, ["id", "user", "amount", "createTime", "payed", "payTime"], renders);
+
+  if (!isNew) {
+    table.replaceChild(rowElem, findOne(table, 0, data.id + ''));
+  } else {
+    table.appendChild(rowElem);
+  }
+}
+
+function renderUser(user) {
+  return `${user.name}(${user.realName})`;
+}
+
+function renderPayed(payed) {
+  return payed ? "支付过" : "还未支付";
+}
+
+function renderAll(data) {
+  var table = document.getElementById("table");
+  var tableTitle = table.firstElementChild.cloneNode(true);
+  table.innerHTML = "";
+  table.appendChild(tableTitle);
+
+  var handlers = [{
+    text: '已支付',
+    handler: onPay
+  }];
+
+  var renders = [null, {
+    innerHTML: renderUser
+  }, null, null, {
+      innerHTML: renderPayed
+    }];
+
+  data.forEach(function (rowData) {
+    var rowElem = buildRow(handlers, rowData, ["id", "user", "amount", "createTime", "payed", "payTime"], renders);
+    table.appendChild(rowElem);
+  });
+}
+
+(function () {
+  get('/tbk/withdraw/request/list').done(responseMapper(renderAll));
+})();
