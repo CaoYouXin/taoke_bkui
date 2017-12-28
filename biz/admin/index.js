b895176cd676b3f0a8a13f1a87dc903d27e3291f
曹又心 <c_lisheng@163.com>
2017-12-28 14:19:33 +0800

    admin management

--- biz/admin/index.js
+++ biz/admin/index.js
@@ -1,6 +1,8 @@
 var table = document.getElementById("table");
 var roles1 = document.getElementById("roles-1");
 var role1 = document.getElementById("role-1");
+var roles2 = document.getElementById("roles-2");
+var role2 = document.getElementById("role-2");
 
 var handlers = [{
   text: '角色设定',
@@ -22,15 +24,44 @@ var renders = [
 var colNames = ["id", "name", "role"];
 
 function onChangeRole(e, data) {
+  letJSONtoForm(data, $('#table-form-3'));
+  $('.table-modal-lg-3').modal({ show: true });
+}
 
+function onRoleChangeSubmit() {
+  var submitData = getJSONfromForm($('#table-form-3'));
+  submitData.role = { id: submitData.roleId };
+  delete submitData.roleId;
+
+  post('/admin/user/role/change', submitData)
+    .done(responseMapper((data) => {
+
+      renderOne(false, data);
+    }));
 }
 
 function onResetPwd(e, data) {
+  letJSONtoForm(data, $('#table-form-2'));
+  $('#table-form-2 input[name="pwd"]').val('');
+  $('.table-modal-lg-2').modal({ show: true });
+}
 
+function onPwdResetSubmit() {
+  var submitData = getJSONfromForm($('#table-form-2'));
+  submitData.pwd = md5(submitData.pwd);
+
+  post('/admin/user/pwd/change', submitData)
+    .done(responseMapper((data) => {
+
+      renderOne(false, data);
+    }));
 }
 
 function onDelete(e, data) {
-
+  post(`/admin/user/delete/${data.id}`).done(responseMapper(() => {
+    var table = document.getElementById("table");
+    table.removeChild(findOne(table, 0, data.id + ''));
+  }));
 }
 
 function renderRole(role) {
@@ -38,12 +69,13 @@ function renderRole(role) {
 }
 
 function onCreateSubmit() {
-  var submitData = getJSONfromForm($('#table-form'));
+  var submitData = getJSONfromForm($('#table-form-1'));
   if (!submitData.id) {
     delete submitData.id;
   } else {
     submitData.id = Number(submitData.id);
   }
+  submitData.pwd = md5(submitData.pwd);
   submitData.roleId = Number(submitData.roleId);
 
   post('/admin/user/create', submitData)
@@ -81,6 +113,7 @@ function buildMenu(name, id) {
   a.innerText = name;
   a.onclick = function (e) {
     $(role1).val(id);
+    $(role2).val(id);
   };
   return a;
 }
@@ -91,6 +124,7 @@ function buildMenu(name, id) {
     roles1.innerHTML = '';
     data.forEach(datum => {
       roles1.appendChild(buildMenu(datum.name, datum.id));
+      roles2.appendChild(buildMenu(datum.name, datum.id));
     });
   }));
 })();
