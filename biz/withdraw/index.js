59230988d09d7c6239870c93a6b1f1fa3b629e52
曹又心 <c_lisheng@163.com>
2018-01-20 11:36:53 +0800

    提现申请监控分类查看

--- biz/withdraw/index.js
+++ biz/withdraw/index.js
@@ -1,3 +1,43 @@
+var params = {};
+
+function loadTable(page, type) {
+  params.page = page;
+  params.type = type;
+  get(`/tbk/withdraw/request/list/${type}/${page}`).done(responseMapper(renderAll));
+}
+
+function onFirstPageClicked() {
+  loadTable(1, params.type);
+}
+
+function onLastPageClicked() {
+  loadTable(params.total, params.type);
+}
+
+function onPreviousPageClicked() {
+  loadTable(Number(params.page) - 1, params.type);
+}
+
+function onNextPageClicked() {
+  loadTable(Number(params.page) + 1, params.type);
+}
+
+function onTopCateClicked(e, load) {
+  $.each($("#topCates > button"), (i, $btn) => {
+    $btn.classList.remove('btn-primary');
+    $btn.classList.remove('btn-secondary');
+
+    $btn.classList.add('btn-secondary');
+  });
+
+  e.target.classList.remove('btn-secondary');
+  e.target.classList.add('btn-primary');
+
+  if (load) {
+    loadTable(1, e.target.getAttribute('data-rel'));
+  }
+}
+
 function onPay(e, data) {
   if (data.payed) {
     return;
@@ -45,7 +85,7 @@ function renderPayed(payed) {
   return payed ? "支付过" : "还未支付";
 }
 
-function renderAll(data) {
+function renderAll(page) {
   var table = document.getElementById("table");
   var tableTitle = table.firstElementChild.cloneNode(true);
   table.innerHTML = "";
@@ -62,12 +102,44 @@ function renderAll(data) {
       innerHTML: renderPayed
     }];
 
-  data.forEach(function (rowData) {
+  page.content.forEach(function (rowData) {
     var rowElem = buildRow(handlers, rowData, ["id", "user", "amount", "createTime", "payed", "payTime"], renders);
     table.appendChild(rowElem);
   });
+
+  $(`section > .badge`).html(`共${page.totalElements}个`);
+
+  $.each($(`section > .btn-group > button`), (i, $page) => {
+    if (i <= 1) {
+      if (page.first) {
+        $page.setAttribute('disabled', true);
+      } else {
+        $page.removeAttribute('disabled');
+      }
+    } else {
+      if (page.last) {
+        $page.setAttribute('disabled', true);
+      } else {
+        $page.removeAttribute('disabled');
+      }
+    }
+  });
+
+  params.total = page.totalPages;
+  location.hash = Object.keys(params).reduce(
+    function (a, k) { a.push(k + '=' + encodeURIComponent(params[k])); return a; }, []
+  ).join('&');
 }
 
 (function () {
-  get('/tbk/withdraw/request/list').done(responseMapper(renderAll));
+  if (location.hash) {
+    params = decodeURIComponent(location.hash).substring(1).split(/&/).reduce(
+      function (a, kv) { keyV = kv.split(/=/); a[keyV[0]] = keyV[1]; return a; }, {}
+    );
+    console.log('refresh from url hash: ', params);
+    onTopCateClicked({ target: $(`#topCates > button[data-rel=${params.type}]`)[0] }, false);
+    loadTable(params.page, params.type);
+  } else {
+    onTopCateClicked({ target: $("#topCates > button")[0] }, true);
+  }
 })();
